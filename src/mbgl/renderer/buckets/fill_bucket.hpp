911014a3d4e3b88489062481c90ef073adbfedbb
Andrew Hay Kurtz <andrew.hay.kurtz@gmail.com>
2019-09-27 14:37:41 -0700

    Fix consistency of paint and layout properties type alias usage in FillBucket, LineBucket

--- src/mbgl/renderer/buckets/fill_bucket.hpp
+++ src/mbgl/renderer/buckets/fill_bucket.hpp
@@ -18,7 +18,6 @@ class RenderFillLayer;
 class FillBucket final : public Bucket {
 public:
     ~FillBucket() override;
-    using PossiblyEvaluatedPaintProperties = style::FillPaintProperties::PossiblyEvaluated;
     using PossiblyEvaluatedLayoutProperties = style::FillLayoutProperties::PossiblyEvaluated;
 
     FillBucket(const PossiblyEvaluatedLayoutProperties layout,
